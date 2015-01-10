4c23bfa9f36a4c6dceaa8f6f7a58da6d28f4e790
Matthieu Napoli <matthieu@mnapoli.fr>
2015-01-10 14:46:49 +1300

    Website generation with Couscous

--- vagrant/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get update -qq
-
-apt-get install -y --quiet git curl php5-cli php5-curl redis-server
-
-# Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
