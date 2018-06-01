eb924d63f8bfeda7565011289da898cbf433caf0
Matthieu Napoli <matthieu@mnapoli.fr>
2018-06-01 23:24:06 +0200

    Optimize ArrayStorage

--- src/Backend/ArrayStorage.php
+++ src/Backend/ArrayStorage.php
@@ -2,7 +2,6 @@
 
 namespace BlackBox\Backend;
 
-use ArrayIterator;
 use BlackBox\Storage;
 use IteratorAggregate;
 
@@ -15,44 +14,25 @@ use IteratorAggregate;
  */
 class ArrayStorage implements IteratorAggregate, Storage
 {
-    /**
-     * @var array
-     */
     private $storage = [];
 
-    /**
-     * {@inheritdoc}
-     */
     public function get($id)
     {
-        if (! array_key_exists($id, $this->storage)) {
-            return null;
-        }
-
-        return $this->storage[$id];
+        return $this->storage[$id] ?? null;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function set($id, $data)
     {
         $this->storage[$id] = $data;
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function remove($id)
     {
         unset($this->storage[$id]);
     }
 
-    /**
-     * {@inheritdoc}
-     */
     public function getIterator()
     {
-        return new ArrayIterator($this->storage);
+        return yield from $this->storage;
     }
 }
